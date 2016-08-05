f55d0305fc0f50cd171d7317cb209d70b7db5b47
Adam Oswalt <email@adamoswalt.com>
2016-08-05 14:58:22 -0500

    add id increment test names

--- Test_Product.py
+++ Test_Product.py
@@ -7,6 +7,9 @@ class TestProduct(unittest.TestCase):
     def test_create_product(self):
         pass
 
+    def test_next_product_id_increments(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
