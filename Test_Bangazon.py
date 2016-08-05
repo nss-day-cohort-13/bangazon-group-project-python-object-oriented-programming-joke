88755b1d4ef01e2eff621b7102b4f56604d392e1
Adam Oswalt <email@adamoswalt.com>
2016-08-05 14:19:44 -0500

    test names and .gitignore

---
+++ Test_Bangazon.py
@@ -0,0 +1,34 @@
+import unittest
+
+from bangazon import *
+
+class TestBangazon(unittest.TestCase):
+
+    def test_bangazon_creation(self):
+        pass
+
+    def test_create_new_customer(self):
+        pass
+
+    def test_select_active_customer(self):
+        pass
+
+    def test_create_new_order(self):
+        pass
+
+    def test_add_product_to_new_order(self):
+        pass
+
+    def test_add_product_to_existing_order(self):
+        pass
+
+    def test_complete_order(self):
+        pass
+
+    def test_get_popular_products(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Test_EasyIO.py
@@ -0,0 +1,15 @@
+import unittest
+
+from easy_io import *
+
+class TestEasyIO(unittest.TestCase):
+
+    def test_serialize(self):
+        pass
+
+    def test_deserialize(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
