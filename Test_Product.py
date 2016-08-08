c24fcc6ee5489d46f34ee2395958537b9e5d4848
Adam Oswalt <email@adamoswalt.com>
2016-08-08 09:17:16 -0500

    test product

--- Test_Product.py
+++ Test_Product.py
@@ -5,10 +5,18 @@ from order import *
 class TestProduct(unittest.TestCase):
 
     def test_create_product(self):
-        pass
+        product = Product('water', 1.25)
+
+        self.assertNotEqual(product.id, 0)
+        self.assertEqual(product.name, 'water')
+        self.assertEqual(product.price, 1.25)
 
     def test_next_product_id_increments(self):
-        pass
+        Product.next_product_id = 1
+
+        product = Product('water', 1.25)
+
+        self.assertEqual(Product.next_product_id, 2)
 
 
 if __name__ == '__main__':
