0bb9aa204a863884acffb75c1e602151e5adcc96
Hannah Hall <hannahkhall13@gmail.com>
2016-08-08 11:16:12 -0500

    customer class created and tests pass

---
+++ customer.py
@@ -0,0 +1,16 @@
+class Customer:
+    """
+    Creates a new customer with the users information and increments the static
+    variable to change the customer's id
+    """
+    next_customer_id = 1
+
+    def __init__(self, name, address, city, state, zipcode, phone):
+        self.name = name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.phone = phone
+        self.id = Customer.next_customer_id
+        Customer.next_customer_id += 1
