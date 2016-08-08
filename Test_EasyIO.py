4e3733b5230252d9180c771f60469a1303951404
Adam Oswalt <email@adamoswalt.com>
2016-08-08 10:12:14 -0500

    test serialize

--- Test_EasyIO.py
+++ Test_EasyIO.py
@@ -1,13 +1,24 @@
 import unittest
+import pickle
 
 from easy_io import *
 
 class TestEasyIO(unittest.TestCase):
 
     def test_serialize(self):
+        filename = 'testfile_io.dat'
+
+        data = [1, 'b', ['a', 'list']]
+        serialize(data, filename)
+
+        with open(filename, 'rb') as read_file:
+            test_read = pickle.load(read_file)
+            self.assertEqual(test_read, data)
+
+    def test_deserialize_no_file(self):
         pass
 
-    def test_deserialize(self):
+    def test_deserialize_existing(self):
         pass
 
 
