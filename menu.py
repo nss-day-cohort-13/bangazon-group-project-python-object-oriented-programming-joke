649a48be6994cf5c42dce57388ef99e885d806e1
Russell Ries Jr <riesru@gmail.com>
2016-08-09 13:57:57 -0500

    add prompt_customer function

--- menu.py
+++ menu.py
@@ -7,30 +7,38 @@ class Menu:
         self.bang = Bangazon()
 
     def main(self):
-        title = 'Welcome to Bangazon! Command Line Ordering System'
+        while True:
+            title = 'Welcome to Bangazon! Command Line Ordering System'
 
-        heading = '\n'
-        heading += '*' * 54 + '\n'
-        heading += '*' + title.center(52, ' ') + '*' + '\n'
-        heading += '*' * 54 + '\n'
+            heading = '\n'
+            heading += '*' * 54 + '\n'
+            heading += '*' + title.center(52, ' ') + '*' + '\n'
+            heading += '*' * 54 + '\n'
 
-        menu = {
-            '1. Create a customer account': self.prompt_customer,
-            '2. Choose active customer': self.prompt_choose_customer,
-            '3. Create a payment option': self.prompt_create_payment,
-            '4. Add product to shopping cart': self.prompt_add_product,
-            '5. Complete an order': self.prompt_complete_order,
-            '6. See product popularity': self.print_popular_products,
-            '7. Exit Bangazon!': exit
-        }
+            menu = {
+                '1. Create a customer account': self.prompt_customer,
+                '2. Choose active customer': self.prompt_choose_customer,
+                '3. Create a payment option': self.prompt_create_payment,
+                '4. Add product to shopping cart': self.prompt_add_product,
+                '5. Complete an order': self.prompt_complete_order,
+                '6. See product popularity': self.print_popular_products,
+                '7. Exit Bangazon!': exit
+            }
 
-        prompt_message = 'Choose an option'
+            prompt_message = 'Choose an option'
 
-        choice = show_menu(heading, menu, prompt_message)
-        choice()
+            choice = show_menu(heading, menu, prompt_message)
+            choice()
 
     def prompt_customer(self):
-        pass
+        name = prompt("Enter Customer Name")
+        address = prompt("Enter Street Name")
+        city = prompt("Enter City")
+        state = prompt("Enter State")
+        zipcode = prompt("Enter Zip Code")
+        phone = prompt("Enter Phone Number")
+        self.bang.create_new_user(name, address, city, state, zipcode, phone)
+        print('Your new user has been created')
 
     def prompt_choose_customer(self):
         pass
