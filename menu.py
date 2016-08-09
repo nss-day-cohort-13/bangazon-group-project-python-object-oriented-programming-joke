0271207db5f9d8f68a57622400fccccc2663b6f7
Adam Oswalt <email@adamoswalt.com>
2016-08-09 13:24:34 -0500

    menu boilerplate

---
+++ menu.py
@@ -0,0 +1,14 @@
+from bangazon import *
+from prompt import *
+
+class Menu:
+
+    def __init__(self):
+        self.bang = Bangazon()
+
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+    Menu().main()
