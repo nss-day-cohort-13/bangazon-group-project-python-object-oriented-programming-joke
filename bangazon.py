6933009e83fd5a544609c37b9a8a6b67e2124fb3
whitney-mitchell <whitney.mitchell84@gmail.com>
2016-08-09 11:34:24 -0500

    Got bangazon test_create_new_customer and test_complete_order passing.

--- bangazon.py
+++ bangazon.py
@@ -15,3 +15,17 @@ class Bangazon(object):
 
         self.active_customer_id = 0
         self.active_order_id = 0
+
+    def create_new_user(self, name, address, city, state, zipcode, phone):
+        new_cust = Customer(name, address, city, state, zipcode, phone)
+
+        self.customers[new_cust.id] = new_cust
+
+    def pay_order(self):
+        if self.active_order_id == 0:
+            return
+
+        active_order = self.orders[self.active_order_id]
+
+        active_order.is_paid = True
+
