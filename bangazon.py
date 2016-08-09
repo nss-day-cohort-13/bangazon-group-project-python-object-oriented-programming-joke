d89d4b7321a2ca2a045af000edc3ac9272563bab
Hannah Hall <hannahkhall13@gmail.com>
2016-08-09 11:09:24 -0500

    functions for creating new order and adding product to order

--- bangazon.py
+++ bangazon.py
@@ -15,3 +15,13 @@ class Bangazon(object):
 
         self.active_customer_id = 0
         self.active_order_id = 0
+
+    def create_new_order(self, customer_id, payment_option_id):
+        if self.active_order_id == 0:
+            new_order = Order(customer_id, payment_option_id=0, is_paid=False)
+            self.orders[new_order.id] = new_order
+            self.active_order_id = new_order.id
+
+    def add_product_to_order(self, order_id, product_id):
+        new_line_item = OrderLineItem(order_id, product_id)
+        self.order_line_items[new_line_item.id] = new_line_item
