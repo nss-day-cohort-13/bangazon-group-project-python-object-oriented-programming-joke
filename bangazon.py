2e2f8995f44a1bd94b0d669ebc7abdd2cdafdd22
whitney-mitchell <whitney.mitchell84@gmail.com>
2016-08-09 15:07:59 -0500

    Really finished bangazon.py doc strings this time.

--- bangazon.py
+++ bangazon.py
@@ -6,6 +6,19 @@ from order import *
 from order_line_item import *
 
 class Bangazon(object):
+    """
+    Contain the logic for all functionality accessible through the user interface, including
+    creating a new user, paying an order, creating a new order, adding a product to an order,
+    selecting an active customer, creating a new payment, and getting popular products.
+
+    Static properties:
+        customers_filename          file storing all customer data
+        payment_options_filename    file storing all payment options data
+        products_filename           file storing all product data
+        orders_filename             file storing all order data
+        line_items_filename         file storing all line item data
+    """
+
     customers_filename = 'customers.dat'
     payment_options_filename = 'payment_options.dat'
     products_filename = 'products.dat'
@@ -13,11 +26,17 @@ class Bangazon(object):
     line_items_filename = 'line_items.dat'
 
     def __init__(self):
-        self.customers = deserialize(Bangazon.customers_filename ,dict())
-        self.payment_options = deserialize(Bangazon.payment_options_filename ,dict())
-        self.products = deserialize(Bangazon.products_filename ,dict())
-        self.orders = deserialize(Bangazon.orders_filename ,dict())
-        self.order_line_items = deserialize(Bangazon.line_items_filename ,dict())
+        """
+        Initialize bangazon, deserialize data files for customers, payment options, products, orders, and line items
+
+
+        """
+
+        self.customers = deserialize(Bangazon.customers_filename, dict())
+        self.payment_options = deserialize(Bangazon.payment_options_filename, dict())
+        self.products = deserialize(Bangazon.products_filename, dict())
+        self.orders = deserialize(Bangazon.orders_filename, dict())
+        self.order_line_items = deserialize(Bangazon.line_items_filename, dict())
 
         self.active_customer_id = 0
         self.active_order_id = 0
@@ -34,6 +53,7 @@ class Bangazon(object):
             zipcode     new customer's zip code
             phone       new customer's phone number
         """
+
         new_cust = Customer(name, address, city, state, zipcode, phone)
         self.customers[new_cust.id] = new_cust
         serialize(self.customers, Bangazon.customers_filename)
@@ -43,6 +63,7 @@ class Bangazon(object):
         Pay/close an open order
 
         """
+
         if self.active_order_id == 0:
             return
 
@@ -58,6 +79,7 @@ class Bangazon(object):
             customer_id         the id of the active customer
             payment_option_id   the id of the selected payment option
         """
+
         if self.active_order_id == 0:
             new_order = Order(customer_id, payment_option_id=0, is_paid=False)
             self.orders[new_order.id] = new_order
@@ -72,6 +94,7 @@ class Bangazon(object):
             order_id     the id of the current order
             product_id   the id of the selected product
         """
+
         new_line_item = OrderLineItem(order_id, product_id)
         self.order_line_items[new_line_item.id] = new_line_item
         serialize(self.order_line_items, Bangazon.line_items_filename)
@@ -139,12 +162,14 @@ class Bangazon(object):
         return popular
 
     def create_new_payment(self, payment_type, account_number, customer_id):
-        """Create New Payment
+        """
+        Create New Payment
 
         Arguments:
-        payment_type        ie. visa, mastercard
-        account_number      number associated with account_number
-        customer_id         id number of active customer"""
+            payment_type        ie. visa, mastercard
+            account_number      number associated with account_number
+            customer_id         id number of active customer
+        """
 
         if self.active_customer_id == 0:
             return
