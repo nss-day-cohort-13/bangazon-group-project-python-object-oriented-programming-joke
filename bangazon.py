0eb16bb580d8261649fc75f5dafdca0cdf2541ac
Adam Oswalt <email@adamoswalt.com>
2016-08-17 14:40:47 -0500

    bangazon conversion

--- bangazon.py
+++ bangazon.py
@@ -1,46 +1,16 @@
-from easy_io import *
-from customer import *
-from payment_option import *
-from product import *
-from order import *
-from order_line_item import *
+import sql
 
 class Bangazon(object):
     """
     Contain the logic for all functionality accessible through the user interface, including
     creating a new user, paying an order, creating a new order, adding a product to an order,
     selecting an active customer, creating a new payment, and getting popular products.
-
-    Static properties:
-        customers_filename          file storing all customer data
-        payment_options_filename    file storing all payment options data
-        products_filename           file storing all product data
-        orders_filename             file storing all order data
-        line_items_filename         file storing all line item data
     """
-
-    customers_filename = 'customers.dat'
-    payment_options_filename = 'payment_options.dat'
-    products_filename = 'products.dat'
-    orders_filename = 'orders.dat'
-    line_items_filename = 'line_items.dat'
 
     def __init__(self):
         """
-        Initialize bangazon, deserialize data files for customers, payment options, products, orders, and line items
+        Initialize active_customer_id and active_order_id to 0
         """
-
-        self.customers = deserialize(Bangazon.customers_filename, dict())
-        self.payment_options = deserialize(Bangazon.payment_options_filename, dict())
-        self.products = deserialize(Bangazon.products_filename, dict())
-        self.orders = deserialize(Bangazon.orders_filename, dict())
-        self.order_line_items = deserialize(Bangazon.line_items_filename, dict())
-
-        Customer.next_customer_id = len(self.customers) + 1
-        PaymentOption.next_payment_option_id = len(self.payment_options) + 1
-        Product.next_product_id = len(self.products) + 1
-        Order.next_order_id = len(self.orders) + 1
-        OrderLineItem.next_line_item_id = len(self.order_line_items) + 1
 
         self.active_customer_id = 0
         self.active_order_id = 0
@@ -58,9 +28,7 @@ class Bangazon(object):
             phone       new customer's phone number
         """
 
-        new_cust = Customer(name, address, city, state, zipcode, phone)
-        self.customers[new_cust.id] = new_cust
-        serialize(self.customers, Bangazon.customers_filename)
+        sql.create_new_user(name, address, city, state, zipcode, phone)
 
     def create_new_payment(self, payment_type, account_number, customer_id):
         """Create New Payment
@@ -72,9 +40,7 @@ class Bangazon(object):
 
         if self.active_customer_id == 0: return
 
-        new_payment = PaymentOption(payment_type, account_number, customer_id)
-        self.payment_options[new_payment.id] = new_payment
-        serialize(self.payment_options, Bangazon.payment_options_filename)
+        sql.insert_new_payment_option(payment_type, account_number, customer_id)
 
     def pay_order(self, payment_option_id):
         """
@@ -83,11 +49,8 @@ class Bangazon(object):
 
         if self.active_order_id == 0: return
 
-        active_order = self.orders[self.active_order_id]
-        active_order.payment_option_id = payment_option_id
-        active_order.is_paid = True
+        sql.update_complete_order(self.active_order_id, payment_option_id)
         self.active_order_id = 0
-        serialize(self.orders, Bangazon.orders_filename)
 
     def create_new_order(self, customer_id):
         """
@@ -99,10 +62,7 @@ class Bangazon(object):
         """
 
         if self.active_order_id == 0:
-            new_order = Order(customer_id, payment_option_id=0, is_paid=False)
-            self.orders[new_order.id] = new_order
-            self.active_order_id = new_order.id
-            serialize(self.orders, Bangazon.orders_filename)
+            self.active_order_id = sql.insert_new_order(customer_id)
 
     def add_product_to_order(self, order_id, product_id):
         """
@@ -113,9 +73,7 @@ class Bangazon(object):
             product_id   the id of the selected product
         """
 
-        new_line_item = OrderLineItem(order_id, product_id)
-        self.order_line_items[new_line_item.id] = new_line_item
-        serialize(self.order_line_items, Bangazon.line_items_filename)
+        sql.insert_new_line_item(order_id, product_id)
 
     def select_active_customer(self, customer_id):
         """
@@ -128,13 +86,10 @@ class Bangazon(object):
         self.active_customer_id = customer_id
 
         # set active order if unfinished order exists
-        active_customer_orders = [
-            order.id
-            for order in self.orders.values()
-            if order.customer_id == self.active_customer_id and
-                not self.orders[order.id].is_paid]
-        if len(active_customer_orders) > 0:
-            self.active_order_id = active_customer_orders[-1]
+        active_customer_orders = sql.select_customer_unpaid_orders(customer_id)
+        self.active_order_id = (active_customer_orders[0]
+                                if len(active_customer_orders) > 0
+                                else 0)
 
     def get_popular_products(self):
         """
@@ -144,39 +99,9 @@ class Bangazon(object):
             dictionary with totals and individual products
         """
 
-        # collect data for each product individually
-        popular = {'products': []}
-        for prod in self.products.values():
-            popular['products'].append({
-                'name': prod.name,
-                'product_id': prod.id,
-                'count': len([
-                    li.id 
-                    for li in self.order_line_items.values()
-                    if li.product_id == prod.id]),
-                'order_count': len({
-                    li.order_id
-                    for li in self.order_line_items.values()
-                    if li.product_id == prod.id}),
-                'customer_count': len({
-                    self.orders[li.order_id].customer_id
-                    for li in self.order_line_items.values()
-                    if li.product_id == prod.id}),
-                'revenue': sum([
-                    prod.price
-                    for li in self.order_line_items.values()
-                    if li.product_id == prod.id])
-            })
-
-        # sort products by order_count
-        popular['products'].sort(key=lambda product: product['order_count'],
-                                 reverse=True)
-
-        # store totalsuu
-        popular['totals'] = {
-            'order_sum': sum([p['order_count'] for p in popular['products']]),
-            'customer_sum': sum([p['customer_count'] for p in popular['products']]),
-            'revenue_sum': sum([p['revenue'] for p in popular['products']])
+        return {
+            # name, product_id, order_count, customer_count, revenue
+            'products': sql.select_popular_products(),
+            # order_sum, customer_sum, revenue_sum
+            'totals': sql.select_popular_totals()
         }
-
-        return popular
