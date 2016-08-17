46ea9de91a57a31878c51b19a5c7fc43af0eda12
Adam Oswalt <email@adamoswalt.com>
2016-08-17 10:57:16 -0500

    run statement function

--- sql.py
+++ sql.py
@@ -1,13 +1,13 @@
 import sqlite3
 
-def run_statement(statement, parameters=None, fetch_amount=0):
+def run_statement(statement, parameters=(), fetch_amount=0):
     """
     Run a statment against the database
 
     Arguments:
         statement               the statement string to be executed
         parameters (optional)   a tuple of statement parameters
-            defaults to None
+            defaults to empty tuple
         fetch_amount (optional) the number of records to return
                                  -1: fetch all
                                   0: none (default)
@@ -17,3 +17,17 @@ def run_statement(statement, parameters=None, fetch_amount=0):
     Returns:
         the fetched result of the statement
     """
+
+    with sqlite3.connect('bangazon.db') as conn:
+            c = conn.cursor()
+            c.execute(statement, parameters)
+            conn.commit()
+
+            # return resulte of appropriate fetch function
+            if fetch_amount:
+                if fetch_amount == -1:
+                    return c.fetchall()
+                elif fetch_amount == 1:
+                    return c.fetchone()
+                else:
+                    return c.fetchmany(fetch_amount)
